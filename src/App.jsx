afb38237d163ad1cb39306bad9527411e94d7be5
taneyayuuka-web <taneya.yuuka@g.wakayama-u.ac.jp>
2026-03-15 21:43:49 +0900

    profile update

--- src/App.jsx
+++ src/App.jsx
@@ -97,7 +97,8 @@ export default function App() {
 
 
       <section className="graph">
-        <h1>システムによる効率的かつ的確なデザイン開発を目指して今、できること</h1>
+        <h1>システムによる効率的かつ的確なデザイン開発を目指して</h1>
+        <h1>今、できること</h1>
         <div className="photo2">
           <img src="/covers/profile2.png" alt="my icon" className="photoimg" />
         </div>
