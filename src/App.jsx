721cece229f219f239fe9956db24212a90065df5
taneyayuuka-web <taneya.yuuka@g.wakayama-u.ac.jp>
2026-05-11 00:38:15 +0900

    book update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 const books = [
   { 
     id: 1, 
-    title: "心蕾", 
+    title: "心o蕾", 
     cover: "/covers/book.png", 
     innerCover: "/covers/book-inner.png", 
     content:
